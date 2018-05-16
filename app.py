bd7c7aafedd244fe30bf5e5675457ac5c8fe3bfd
Samuela Pollack <sampoll@users.noreply.github.com>
2018-05-16 17:40:47 -0400

    numpy slice subscripts implemented

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 api = Api(app)
 
 domain = 'testfile.testdir.testh5serv.org'
-endpoint = 'http://54.167.16.161:5000'
+endpoint = 'http://107.23.2.183:5000'       # Note: need to change this every time test server is relaunched
 f = h5pyd.File(domain, 'r', endpoint)
 
 @api.route('/path/<path:dsetname>')
@@ -32,6 +32,10 @@ parser.add_argument('elements')
 # Question:
 # What can be passed to '[' function in h5pyd? numpy arrays? How does d[0:10, 0:10, 0:10] work,
 #   and why doesn't it work here?
+# Answer: indices are of type numpy.slice
+#   0:10:1 is shorthand for slice(0, 10, 1)
+#   expect 'elements' to be a comma-separated string of the form start:stop:step
+# E.g., elements: 0:4:2,0:3:1,0:6:3
 
 @api.route('/data')
 class HDF5Data(Resource):
@@ -42,15 +46,26 @@ class HDF5Data(Resource):
       p = args['path']
       pth = f[p] 
       E = args['elements'].split(',')      # E is an array of string
-      if (len(pth.shape) == 3):            # generalize to arbitrary dimension
-        res = pth[int(E[0]), int(E[1]), int(E[2])]
-      elif (len(pth.shape) == 2):
-        res = pth[int(E[0]), int(E[1])]
-      elif (len(pth.shape) == 1):
-        res = pth[int(E[0])]
-      print("Result = %s" % res)
-    except:
-      return { 'result' : 'error' }
+      slices = []
+
+      for ee in E:
+        (s0, s1, s2) = ee.split(':')
+        slices.append(slice(int(s0), int(s1), int(s2)))
+
+      if (len(slices) == len(pth.shape)):
+        if (len(pth.shape) == 3):            # generalize to arbitrary dimension
+          res = pth[slices[0], slices[1], slices[2]]
+        elif (len(pth.shape) == 2):
+          res = pth[slices[0], slices[1]]
+        elif (len(pth.shape) == 1):
+          res = pth[slices[0]] 
+        else:
+          raise ValueError('Bad slice')
+      else:
+        raise ValueError('Bad slice')
+      
+    except ValueError as err:
+      return { 'result' : 'slice-syntax-error' }
     return { 'path' : args['path'], 'elements' : args['elements'] , 'output' : str(res) } 
 
 
